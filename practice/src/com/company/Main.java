6dc6c75b3222577bf72b0334060f7c602be02284
soli21 <m.solih.ms@gmail.com>
2021-03-18 11:31:37 +0600

    second try

--- practice/src/com/company/Main.java
+++ practice/src/com/company/Main.java
@@ -7,6 +7,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+        char operation = scanner.next().charAt(0);
         System.out.println("Eki san jazynyz");
     }
 }
