2c8d87f86690e8ad8a9313a035bbdfb1c8e1a0f5
soli21 <m.solih.ms@gmail.com>
2021-03-18 11:21:17 +0600

    first try

--- practice/src/com/company/Main.java
+++ practice/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Eki san jazynyz");
     }
 }
